63d4bce42b0c4c2434cd490ac1b366d15e480e5d
SrMulaji <99091584+SrMulaji@users.noreply.github.com>
2022-11-29 19:36:34 +0200

    Initial commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div class="pull-right">
+		<footer>
+           <p>By your favourite psuedonym Admin Nimda</p>
+    </footer>
+</div>
---
+++ title_index.php
@@ -0,0 +1,38 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+                                    <p class="e-learning">E-Learning System by the <strong>"MAN IN THE MIRROR"</strong></p>
+							<h3>
+
+							<p>E-Learning System</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>E-Learning EXCELS:</p>
+												<p>Excellence, Competence and Educational</p>
+												<p>Leadership in Science and Technology</p>
+								</div>		
+						</div>		
+				</div>
